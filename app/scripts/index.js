f7822825a55d6def03949746574363fde078ae65
Ryan Barron <ryanbarron243@gmail.com>
2017-01-31 21:09:38 -0500

    first commit

---
+++ app/scripts/index.js
@@ -0,0 +1,76 @@
+var handlebars= require("handlebars");
+var $ = require('jquery');
+var underscore = require('underscore');
+
+var source=$('.tile-template').html();
+var template = handlebars.compile(source);
+$('.template-container').append(template());
+
+var url = "https://api.etsy.com/v2/listings/active.js?api_key=cdwxq4soa7q4zuavbtynj8wx&keywords=yarn&includes=Images,Shop";
+
+
+fetchJSONP(url, function(data) {
+  // do something with data
+  data.results.forEach(function(item){
+    var itemInfo = {
+      title: item.title
+    };
+    console.log(item.title);
+    $('.template-container').append(template(itemInfo));
+
+  });
+  console.log(data.results);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+  (url: String, callback: Function) -> undefined
+
+  Execute a callback function with the JSON results from the url specified.
+
+  Examples
+      var url = "https://api.etsy.com/v2/listings/active.js?api_key=cdwxq4soa7q4zuavbtynj8wx&keywords=yarn&includes=Images,Shop";
+
+      fetchJSONP(url, function(data) {
+        // do something with data
+      });
+
+      // OR
+
+      function logData(data) {
+        console.log(data);
+      }
+
+      fetchJSONP(url, logData);
+*/
+function fetchJSONP(url, callback) {
+    var callbackName = 'jsonp_callback_' + Math.round(100000 * Math.random());
+    var script = document.createElement('script');
+
+    window[callbackName] = function(data) {
+        delete window[callbackName];
+        document.body.removeChild(script);
+        callback(data);
+    };
+
+    script.src = url + (url.indexOf('?') >= 0 ? '&' : '?') + 'callback=' + callbackName;
+    document.body.appendChild(script);
+}
